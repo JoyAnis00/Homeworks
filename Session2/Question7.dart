da86838a7cf2a4deddc3249e3f1a7717432041d6
Joy Anis00 <joyanis00l@gmail.com>
2025-03-09 12:27:54 +0200

    Fix file renaming and add new questions for Session2

---
+++ Session2/Question7.dart
@@ -0,0 +1,10 @@
+//  Practice conditional statements with integer values.
+//Instructions:- Create an integer variable marks.- If marks is 70 or higher, print 'Passed', otherwise print 'Failed'
+void main() {
+  int marks = 75;
+  if (marks >= 70) {
+    print('Passed');
+  } else {
+    print('Failed');
+  }
+}
